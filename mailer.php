a0a8371811f8b1985881622b60e2d575c0db2ab9
Yvo <y.vanoosterum@gmail.com>
2017-07-13 15:20:27 +0200

    improved the about area

--- mailer.php
+++ mailer.php
@@ -7,13 +7,16 @@
         $email_field = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];
-        $message2 = "Thank you for your email. I will contact you as soon as possible! Kind regards, Yvo";
+        $message2 = "Thank you for your email. I will contact you as soon as possible!\n \n Kind regards, \n \n Yvo";
 
         $body = "From: $name_field\n E-Mail: $email_field\n Subject: $subject\n Message:\n $message";
 
         echo "Data has been submitted to $to!";
         mail($to, $subject, $body);
         mail($email_field, $subject, $message2);
+
+        header("Location: http://www.yvvs.nl/");
+        die();
 
         } else {
 
